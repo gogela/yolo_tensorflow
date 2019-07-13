627f31725dbe99e3a7c59c652a614f9a606e6487
gogela <gogela@atlas.cz>
2019-07-13 10:14:34 +0200

    reverting classes

--- yolo/config.py
+++ yolo/config.py
@@ -17,11 +17,11 @@ WEIGHTS_DIR = os.path.join(DATA_PATH, 'weights')
 #WEIGHTS_FILE = None
 WEIGHTS_FILE = os.path.join(DATA_PATH, 'weights', 'YOLO_small.ckpt')
 
-#CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-#           'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
-#           'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
-#           'train', 'tvmonitor']
-CLASSES=['DISPLAY']
+CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+           'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
+           'train', 'tvmonitor']
+#CLASSES=['DISPLAY']
 
 FLIPPED = True
 
